888efecdc2798fefd2ba37116c1678e8ae6416f6
amandatlt <amanda.teo@nera.com>
2017-02-14 12:46:31 +0000

    updated ggplot graph - basic graph done

--- Scripts/test-marey-chart.R
+++ Scripts/test-marey-chart.R
@@ -16,6 +16,8 @@ datafile <- loadWorkbook(paste0(base.dir,"/Data", "/test-data.xlsx"))
 df <- readWorksheet(datafile, "Sheet1")
 
 ### ---clean data--- ###
+
+#convert visit type to numeric (not important)
 convert.visits <- function(x) {
   y <- 1
   if (x == "AE") y <- 2
@@ -24,16 +26,26 @@ convert.visits <- function(x) {
 }
 df[, c("visit_type_num")] <- sapply(df[,"visit_type"], convert.visits)
 
+#set visit_type to factor (so ggplot recognises the full range of discrete x axis)
+df[,"visit_type"] <- as.factor(df$visit_type)
+
 ### ---plot data---###
 
 #will plot visit type over time
 #time period of one year
 
-#simple bar diagram
-df.plot <- df[df$patientid == "A7777",]
+#simple scatter plot diagram
+ggplot(subset(df, patientid == "X011"), aes(x=visit_type, y = date, colour = visit_type)) + 
+  geom_point() +
+  scale_y_continuous(name = "day of patient journey", trans = "reverse", limits = c(365,1)) +
+  scale_x_discrete(drop = FALSE) +
+  theme(panel.border = element_blank(),
+        panel.background = element_blank(),
+        axis.line = element_line( size=.1, color="black"),
+        panel.grid.major.y = element_blank(),
+        panel.grid.major.x = element_line( size=.1, color="light grey"))
 
+#points too small
+#x axis looks too wide for now
 
-df.fulldates <- as.data.frame(cbind(rep("A777", 365), 1:365), 
-                              col.names = c("patientid", "date"),
-                              stringsAsFactors = FALSE)
-ggplot(df.plot, aes(x = date, y = visit_type_num)) + geom_bar(stat = "identity")
+#layer on text describing journey
