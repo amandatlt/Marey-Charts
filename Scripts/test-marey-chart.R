149173bfdcc5f57622e25b462387739455a69250
amandatlt <amanda.teo@nera.com>
2017-02-15 11:34:04 +0000

    Added specialty descrip. to scatterpoints

--- Scripts/test-marey-chart.R
+++ Scripts/test-marey-chart.R
@@ -9,6 +9,7 @@
 .libPaths("P:/User/Amanda.Teo/R/Packages")
 library(XLConnect)
 library(ggplot2)
+library(ggrepel)
 
 ### ---load data---###
 base.dir <- dirname(getwd())
@@ -29,23 +30,35 @@ df[, c("visit_type_num")] <- sapply(df[,"visit_type"], convert.visits)
 #set visit_type to factor (so ggplot recognises the full range of discrete x axis)
 df[,"visit_type"] <- as.factor(df$visit_type)
 
+#replace all specialities and ep_icd10 with spaces if there is NA
+df[c("ep_icd10", "Specialty")][is.na(df[c("ep_icd10", "Specialty")])] <- ""
+
 ### ---plot data---###
 
 #will plot visit type over time
 #time period of one year
 
 #simple scatter plot diagram
-ggplot(subset(df, patientid == "X011"), aes(x=visit_type, y = date, colour = visit_type)) + 
-  geom_point() +
-  scale_y_continuous(name = "day of patient journey", trans = "reverse", limits = c(365,1)) +
-  scale_x_discrete(drop = FALSE) +
-  theme(panel.border = element_blank(),
-        panel.background = element_blank(),
-        axis.line = element_line( size=.1, color="black"),
-        panel.grid.major.y = element_blank(),
-        panel.grid.major.x = element_line( size=.1, color="light grey"))
-
+df.plot <- subset(df, patientid == "C4453")
+g <- ggplot(df.plot, aes(x=visit_type, y = date, colour = visit_type)) + 
+      geom_point() +
+      scale_y_continuous(name = "day of patient journey", trans = "reverse", limits = c(365,1)) +
+      scale_x_discrete(drop = FALSE) +
+      theme(panel.border = element_blank(),
+            panel.background = element_blank(),
+            axis.line = element_line( size=.1, color="black"),
+            panel.grid.major.y = element_blank(),
+            panel.grid.major.x = element_line( size=.1, color="light grey"))
+g
 #points too small
 #x axis looks too wide for now
 
+#layer on specialty with repel
+g + geom_label_repel(aes(label =Specialty, fill = Specialty),
+                     colour = "white",
+                     size = 3.5, 
+                     nudge_x = 0.2,
+                     show.legend = FALSE)
+
 #layer on text describing journey
+#use geom_text_repel
